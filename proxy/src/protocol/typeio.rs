388292c6ef3920cf699cb1348db477f242c19d42
MichaAI <michaai@neodustry.ru>
2025-10-14 15:32:58 +0300

    TypeIOString rewrite

--- proxy/src/protocol/typeio.rs
+++ proxy/src/protocol/typeio.rs
@@ -1,39 +1,64 @@
-use std::{borrow::Cow, result};
+use binrw::{BinRead, BinResult, BinWrite, binrw};
+use cesu8::to_java_cesu8;
 
-use bytes::{Buf, BufMut, Bytes, BytesMut};
-use cesu8::{Cesu8DecodingError, from_java_cesu8, to_java_cesu8};
-use thiserror::Error;
+#[binrw]
+#[brw(big)]
+#[derive(Debug, Clone)]
+pub struct TypeIOString {
+    pub exists: u8,
 
-pub struct TypeIOString {}
-
-#[derive(Debug, Error)]
-pub enum TypeIOStringDecodeError {
-    #[error("Can't decode cesu8 string")]
-    BadCesu8(#[from] Cesu8DecodingError),
+    #[br(if(exists != 0))]
+    #[bw(if(*exists != 0))]
+    #[br(parse_with = parce_cesu8_optional, args(exists))]
+    #[bw(write_with = write_cesu8, args(exists))]
+    pub value: Option<String>,
 }
 
-impl TypeIOString {
-    pub fn encode(string: &str, buf: &mut BytesMut) -> Result<(), TypeIOStringDecodeError> {
-        if string.len() == 0 {
-            buf.put_u8(0);
-            return Ok(());
-        }
-        buf.put_u8(1);
-        let cesu8_bytes = to_java_cesu8(string);
-        buf.put_u16(cesu8_bytes.len() as u16);
-        buf.put(&cesu8_bytes[..]);
-        Ok(())
+#[binrw::parser(reader)]
+fn parce_cesu8_optional(exists: u8) -> BinResult<Option<String>> {
+    if exists == 0 {
+        return Ok(None);
+    }
+    let len = u16::read_be(reader)?.into();
+    let mut buf = vec![0u8; len];
+    let str_len = reader.read(&mut buf)?;
+    if len != str_len {
+        return Err(binrw::Error::Custom {
+            pos: 0,
+            err: Box::new(0),
+        });
+    }
+    return Ok(Some(
+        cesu8::from_java_cesu8(&buf)
+            .map_err(|e| binrw::Error::Custom {
+                pos: 0,
+                err: Box::new(e),
+            })?
+            .into_owned(),
+    ));
+}
+
+#[binrw::writer(writer)]
+fn write_cesu8(text: &Option<String>, exists: &u8) -> BinResult<()> {
+    if *exists == 0 {
+        return Ok(());
     }
 
-    pub fn decode(buf: &mut Bytes) -> Result<Option<String>, TypeIOStringDecodeError> {
-        let exists = buf.get_u8();
-        if exists == 0 {
-            return Ok(None);
-        }
-        let len = buf.get_u16();
-        let slice = buf.split_to(len as usize);
-        let res = from_java_cesu8(&slice[..])?;
+    match text {
+        Some(text) => {
+            if text.len() > u16::MAX.into() {
+                let collect: String = text.chars().take(u16::MAX.into()).collect();
+                (collect.len() as u16).write_be(writer);
+                to_java_cesu8(&collect).write_args(writer, ());
+                return Ok(());
+            }
 
-        return Ok(Some(res.into_owned()));
+            (text.len() as u16).write_be(writer)?;
+            to_java_cesu8(text).write_args(writer, ())
+        }
+        None => {
+            0u16.write_be(writer)?;
+            return Ok(());
+        }
     }
 }
